120c08d1eb85225f8c8c30615d5742c643ee2032
Scyu <Scyu_@wynntils.com>
2020-11-10 18:06:57 +0000

    Update dist location

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,8 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://cdn.wynntils.com/launcher/distribution.json'
-        //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
+        const distroURL = 'https://cdn.wynntils.com/launcher/dist.php'
         const opts = {
             url: distroURL,
             timeout: 2500
